29e434b03fce21c8824975c5b1f96a4782703297
tauraamui <adampstringer@protonmail.com>
2023-01-05 02:47:54 +0000

    Add nvim config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- Aesthetic
+-- pcall catches errors if the plugin doesn't load
+local ok, catppuccin = pcall(require, "catppuccin")
+if not ok then return end
+vim.g.catppuccin_flavour = "macchiato"
+catppuccin.setup()
+vim.cmd [[colorscheme catppuccin]]
+
+require('me.lsp')
+require('me.options')
+require('me.globals')
+require('me.lualine')
+require('me.keymap')
+require('me.telescope')
