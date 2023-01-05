38e56a5c5c9be8a51320be204fb1cd1f7314c033
tauraamui <adampstringer@protonmail.com>
2023-01-05 01:43:40 +0000

    Initial commit

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,15 @@
+~/bin/gvm 1.19 | source
+
+
+set -gx PATH $PATH ~/.local/bin
+set -gx PATH $PATH ~/go/bin
+set -gx GPG_TTY (tty)
+
+. $HOME/.shonenjump.fish
+
+alias vim="lvim"
+alias pbcopy="xclip -selection clipboard"
+alias pbpaste="xclip -selection clipboard -o"
+
+starship init fish | source
+
