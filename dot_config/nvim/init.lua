56f0b2f11a7d4f777bb4c7418a7aefa52fb69635
tauraamui <adampstringer@protonmail.com>
2023-01-06 09:47:10 +0000

    Change catppuccin colour scheme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,10 +1,89 @@
 -- Aesthetic
 -- pcall catches errors if the plugin doesn't load
-local ok, catppuccin = pcall(require, "catppuccin")
-if not ok then return end
-vim.g.catppuccin_flavour = "macchiato"
-catppuccin.setup()
-vim.cmd [[colorscheme catppuccin]]
+local present, catppuccin = pcall(require, "catppuccin")
+if not present then return end
+
+catppuccin.setup {
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	term_colors = true,
+	transparent_background = false,
+	no_italic = false,
+	no_bold = false,
+	styles = {
+		comments = {},
+		conditionals = {},
+		loops = {},
+		functions = {},
+		keywords = {},
+		strings = {},
+		variables = {},
+		numbers = {},
+		booleans = {},
+		properties = {},
+		types = {},
+	},
+	color_overrides = {
+		mocha = {
+			base = "#000000",
+		},
+	},
+	highlight_overrides = {
+		mocha = function(C)
+			return {
+				TabLineSel = { bg = C.pink },
+				NvimTreeNormal = { bg = C.none },
+				CmpBorder = { fg = C.surface2 },
+				Pmenu = { bg = C.none },
+				NormalFloat = { bg = C.none },
+				TelescopeBorder = { link = "FloatBorder" },
+			}
+		end,
+	},
+}
+-- require("catppuccin").setup {
+-- 	term_colors = true,
+-- 	transparent_background = false,
+-- 	integrations = {
+-- 		gitsigns = true,
+-- 		neotree = true,
+-- 		notify = true,
+-- 		treesitter = true,
+-- 		treesitter_context = true,
+-- 		symbols_outline = true,
+-- 		telescope = true,
+-- 		lsp_trouble = true,
+-- 		which_key = true,
+-- 		neotest = true,
+-- 		mini = true,
+-- 		noice = true,
+-- 		mason = true,
+-- 		dap = {
+-- 			enabled = true,
+-- 			enable_ui = true, -- enable nvim-dap-ui
+-- 		},
+-- 		native_lsp = {
+-- 			enabled = true,
+-- 			virtual_text = {
+-- 				errors = { "italic" },
+-- 				hints = { "italic" },
+-- 				warnings = { "italic" },
+-- 				information = { "italic" },
+-- 			},
+-- 			underlines = {
+-- 				errors = { "underline" },
+-- 				hints = { "underline" },
+-- 				warnings = { "underline" },
+-- 				information = { "underline" },
+-- 			},
+-- 		},
+-- 		navic = {
+-- 			enabled = false,
+-- 			custom_bg = "NONE",
+-- 		},
+-- 	},
+-- }
+
+vim.cmd.colorscheme "catppuccin"
 
 require('me.nvimtree')
 require('me.lsp')
