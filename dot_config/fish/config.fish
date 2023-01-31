30de5e247c6bde04501a1bfbafedb0e4a0c6f6c6
tauraamui <adampstringer@protonmail.com>
2023-01-31 12:11:07 +0000

    refactor: amend jump setup to reflect new implementation

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,11 +1,10 @@
 ~/bin/gvm 1.19 | source
 
-
 set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/go/bin
 set -gx GPG_TTY (tty)
 
-. $HOME/.shonenjump.fish
+jump shell fish | source
 
 alias vim="lvim"
 alias pbcopy="xclip -selection clipboard"
