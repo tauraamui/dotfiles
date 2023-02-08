8f420b307c32e337a25308be2e83fe75fd227c6d
tauraamui <adampstringer@protonmail.com>
2023-02-08 09:36:50 +0000

    wezterm: disable tab bar since we don't use it

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
     font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' }),
-    color_scheme = 'carbonfox'
+    color_scheme = 'carbonfox',
+    enable_tab_bar = false
 }
 
