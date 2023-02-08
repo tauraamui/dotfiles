f323ba2903fbec15e12a636fe3f64acd9bdde555
tauraamui <adampstringer@protonmail.com>
2023-02-08 17:44:39 +0000

    wezterm: update color scheme to HemisuDark from carbonfox

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
     font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' }),
-    color_scheme = 'carbonfox',
+    color_scheme = 'HemisuDark (Gogh)',
     enable_tab_bar = false
 }
 
