8381a051ffdd5bdeebeddb0e22ecea8f5a1c0979
tauraamui <adampstringer@protonmail.com>
2023-04-18 17:39:20 +0100

    nvim: amend dew landing screen

--- dot_config/nvim/lua/startup/themes/dew.lua
+++ dot_config/nvim/lua/startup/themes/dew.lua
@@ -45,6 +45,18 @@ local settings = {
         default_color = "",
         oldfiles_amount = 0,
     },
+    header_2 = {
+        type = "text",
+        oldfiles_directory = false,
+        align = "center",
+        fold_section = false,
+        title = "Quote",
+        margin = 5,
+        content = require("startup.functions").quote(),
+        highlight = "Constant",
+        default_color = "",
+        oldfiles_amount = 0,
+    },
     -- name which will be displayed and command
     body = {
         type = "mapping",
@@ -55,9 +67,9 @@ local settings = {
         margin = 5,
         content = {
             { " Find File", "Telescope find_files", "<leader>ff" },
-            { " Find Word", "Telescope live_grep", "<leader>lg" },
-            { " Recent Files", "Telescope oldfiles", "<leader>of" },
-            { " File Browser", "Telescope file_browser", "<leader>fb" },
+            { " Find Word", "Telescope live_grep", "<leader>fg" },
+            { " Recent Files", "Telescope oldfiles", "<leader>fo" },
+            { " File Browser", "Telescope file_browser", "<leader>fv" },
             { " Colorschemes", "Telescope colorscheme", "<leader>cs" },
             { " New File", "lua require'startup'.new_file()", "<leader>nf" },
         },
@@ -72,7 +84,7 @@ local settings = {
         fold_section = false,
         title = "Footer",
         margin = 5,
-        content = { "startup.nvim" },
+        content = { "tauraamui's neovim ©" },
         highlight = "Number",
         default_color = "",
         oldfiles_amount = 0,
@@ -81,7 +93,6 @@ local settings = {
     options = {
         mapping_keys = true,
         cursor_column = 0.5,
-        empty_lines_between_mappings = true,
         disable_statuslines = true,
         paddings = { 1, 3, 3, 0 },
     },
@@ -96,6 +107,6 @@ local settings = {
         background = "#1f2227",
         folded_section = "#56b6c2",
     },
-    parts = { "header", "body", "footer" },
+    parts = { "header", "header_2", "body", "footer" },
 }
 return settings
