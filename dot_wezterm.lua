62a370bde992fa8b25e32c22134f2d9a507e66dc
tauraamui <adampstringer@protonmail.com>
2023-04-27 10:53:31 +0100

    wezterm: make setting transparency apply just for macOS machines

--- dot_wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require 'wezterm'
-
-return {
-    font = wezterm.font('Hasklug Nerd Font Mono', { weight = 'DemiBold' }),
-    color_scheme = 'ibm3270 (Gogh)',
-    enable_tab_bar = false
-}
-
