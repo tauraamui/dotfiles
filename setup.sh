7287ee0c4ce285bac4cf29161186de9f2e5a98d9
Adam Stringer <adampstringer@protonmail.com>
2025-09-07 03:25:52 +0100

    auto source
    attempting to automatically be able to correctly resolve nix bin for following commands

--- setup.sh
+++ setup.sh
@@ -1,10 +1,15 @@
+#!/bin/bash
+
 # install nix
 sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
-~/.nix-profile/etc/profile.d/nix.sh
 
 mkdir -p ~/.config/nix
 cp ./nix.conf ~/.config/nix
 cp ./flake.nix ~/
+
+# run the rest in a subshell with nix sourced
+bash -c '
+source ~/.nix-profile/etc/profile.d/nix.sh
 
 nix flake update
 nix build
@@ -13,4 +18,5 @@ nix run home-manager/master -- init
 
 cp ./home.nix ~/.config/home-manager/home.nix
 
-nix run home-manager switch
+nix run home-manager/master -- switch
+'
