3f3184bf9a3d6419b29ee8952f0469db62ed0f77
tauraamui <adampstringer@protonmail.com>
2025-09-07 18:53:06 +0100

    fix: correctly set allow unfree packages

--- flake.nix
+++ flake.nix
@@ -3,31 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    nixpkgs.config.allowUnfree = true;
     home-manager = {
-	url = "github:nix-community/home-manager/release-25.05";
-	inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }:
+  let
+    pkgs = import nixpkgs {
+      system = "x86_64-linux";
+      config.allowUnfree = true;
+    };
+  in {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-linux.hello = pkgs.hello;
 
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
 
     homeConfigurations."tauraamui" = home-manager.lib.homeManagerConfiguration {
-	pkgs = nixpkgs.legacyPackages.x86_64-linux;
+	pkgs = pkgs;
 
 	extraSpecialArgs = {
-            gpgKeyGeneratorScript = import ./pkgs/gpg-key-generator.nix {
-              pkgs = nixpkgs.legacyPackages.x86_64-linux;
-            };
+      gpgKeyGeneratorScript = import ./pkgs/gpg-key-generator.nix {
+        pkgs = pkgs;
+      };
 	};
 
 	modules = [
-          ./home.nix
-        ];
+      ./home.nix
+    ];
     };
   };
 }
