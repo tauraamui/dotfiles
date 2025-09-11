9a8d52ffe58c90285ff1738c75a3f7e5c82cd39d
tauraamui <adampstringer@protonmail.com>
2025-09-11 23:38:58 +0100

    fix: pin nixos nixpkgs to share version of home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
