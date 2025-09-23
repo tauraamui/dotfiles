ecba07397b7595bc0b8d7dd987f36602463b02a0
tauraamui <adampstringer@protonmail.com>
2025-09-23 15:46:03 +0100

    bluetooth: add script to be optionally invoked to resolve choppyness

--- patch-bluetooth.sh
+++ patch-bluetooth.sh
@@ -1,12 +1,19 @@
-/etc/modprobe.d/rtw_8821ce.conf
+#!/bin/bash
 
+# Create rtw_8821ce.conf file
+cat << EOF | sudo tee /etc/modprobe.d/rtw_8821ce.conf
 options rtw88_core disable_lps_deep=y
 options rtw88_pci disable_msi=y disable_aspm=y
 options rtw_core disable_lps_deep=y
 options rtw_pci disable_msi=y disable_aspm=y
+EOF
 
+echo "Created /etc/modprobe.d/rtw_8821ce.conf"
 
-/etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
-
+# Create default-wifi-powersave-on.conf file
+cat << EOF | sudo tee /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
 [connection]
 wifi.powersave = 2
+EOF
+
+echo "Created /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf"
