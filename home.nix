b17b0c5533f5e34f3e357d04843f5569b718edaf
tauraamui <adampstringer@protonmail.com>
2026-04-16 16:54:36 +0100

    fix: better align setup to Linux environment

---
+++ home.nix
@@ -0,0 +1,374 @@
+{ config, pkgs, lib, ... }:
+let
+  crush = pkgs.buildGoModule rec {
+    pname = "crush";
+    version = "0.24.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "charmbracelet";
+      repo = "crush";
+      rev = "v${version}";
+      sha256 = "sha256-UcaWSMBVjIaGG9AhdJtzHCWkkVpzmhN9PPsmeDCxvi4=";
+    };
+
+    vendorHash = "sha256-eKiDfdZqpB2+j4S3KcOswnFum3yPSdPzxp1A80DnxQg=";
+    doCheck = false;
+  };
+
+  invoice = pkgs.buildGoModule {
+    pname = "invoice";
+    version = "0.1.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "maaslalani";
+      repo = "invoice";
+      rev = "main";
+      sha256 = "sha256-nHTwNdc6IvKRYZGeU3PHDb++brgs0YR34GgQFue3+FE=";
+    };
+
+    vendorHash = "sha256-mLn9hN7hd3MPYx0STiwCL8pTTYtDlycVkSLUEq8NZOE=";
+    doCheck = false;
+  };
+in
+{
+  home.username = "tauraamui";
+  home.homeDirectory = "/Users/tauraamui";
+  home.stateVersion = "25.05";
+
+  home.packages = [
+    pkgs.bat
+    pkgs.lazygit
+    pkgs.htop
+    pkgs.gnupg
+    pkgs.pinentry-curses
+    pkgs.gawk
+    pkgs.git
+    pkgs.wget
+    pkgs.vlang
+    pkgs.go
+    pkgs.svu
+    pkgs.gh
+    pkgs.httpie
+    pkgs.go-task
+    # go tools + packages
+    crush
+    pkgs.gotools
+    pkgs.gotestsum
+    pkgs.gofumpt
+    pkgs.sqlc
+    pkgs.scc
+    invoice
+  ];
+
+  home.sessionVariables = {
+    PAGER = "less";
+    EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+  programs.ripgrep.enable = true;
+  programs.gh-dash.enable = true;
+
+  programs.bat = {
+    enable = true;
+    config.theme = "TwoDark";
+  };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        email = "adampstringer@protonmail.com";
+        name = "tauraamui";
+      };
+      url = {
+        "ssh://git@github.com/" = {
+          insteadOf = "https://github.com/";
+        };
+      };
+    };
+    signing = {
+      format = "ssh";
+      key = "/Users/tauraamui/.ssh/id_ed25519.pub";
+      signByDefault = true;
+    };
+  };
+
+  programs.gh = {
+    enable = true;
+    settings = {
+      git_protocol = "ssh";
+    };
+    gitCredentialHelper = {
+      enable = true;
+    };
+  };
+
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "10m";
+      sync_address = "https://api.atuin.sh";
+      search_mode = "fuzzy";
+    };
+  };
+
+  programs.autojump = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    extraLuaConfig = ''${builtins.readFile ./nvim/init.lua}'';
+    plugins = with pkgs.nvimPlugins; [
+      gitsigns
+      nightfox-theme
+      nvim-autopairs
+      nvim-goc
+      nvim-lspconfig
+      nvim-treesitter
+      nvim-tree
+      nvim-web-devicons
+      plenary
+      pkgs.vimPlugins.lazy-lsp-nvim
+      startup
+      telescope
+      telescope-file-browser
+      toggleterm
+      trouble
+      vim-tmux-navigator
+    ];
+
+    extraConfig = ''
+        lua << EOF
+        require('lazy-lsp').setup {}
+        EOF
+    '';
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings.font.normal.family = "GohuFont 11 Nerd Font Mono";
+    settings.font.size = 11;
+  };
+
+  programs.tmux = {
+    enable = true;
+    keyMode = "vi";
+    prefix = "C-w";
+    baseIndex = 1;
+    mouse = true;
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      {
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-plugins "battery ram-usage cpu-usage time weather"
+          set -g @dracula-show-powerline true
+          set -g @dracula-fixed-location "Oxford"
+          set -g @dracula-show-fahrenheit false
+          set -g @dracula-show-flags true
+          set -g @dracula-show-left-icon smiley
+        '';
+      }
+    ];
+    extraConfig = ''${builtins.readFile ./tmux/tmux.conf}'';
+  };
+
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_state"
+        "$git_status"
+        "$c"
+        "$elixir"
+        "$elm"
+        "$golang"
+        "$haskell"
+        "$java"
+        "$julia"
+        "$nodejs"
+        "$nim"
+        "$rust"
+        "$scala"
+        "$docker_context"
+        "$cmd_duration"
+        "$fill"
+        "$time"
+        "$line_break"
+        "$python"
+        "$character"
+      ];
+      add_newline = false;
+      directory = {
+        style = "#75c5fa";
+        fish_style_pwd_dir_length = 1;
+      };
+      character = {
+        success_symbol = "[❯](purple)";
+        error_symbol = "[❯](red)";
+        vimcmd_symbol = "[❮](green)";
+      };
+      c = {
+        symbol = "";
+        style = "fg:#6EB0D4";
+        format = "'[$symbol ($version) ]($style)'";
+      };
+      docker_context = {
+        symbol = "";
+        style = "fg:#6EB0D4";
+        format = "'[$symbol $context ]($style) $path'";
+      };
+      elixir = {
+        symbol = "";
+        style = "fg:#6EB0D4";
+        format = "'[$symbol ($version) ]($style)'";
+      };
+      elm = {
+        symbol = "";
+        style = "fg:#6EB0D4";
+        format = "'[$symbol ($version) ]($style)'";
+      };
+      git_branch = {
+        format = "[$branch]($style)";
+        style = "bright-black";
+      };
+      git_status = {
+        format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed )]($style)";
+        style = "cyan";
+        conflicted = "=";
+        untracked = "⇡";
+        modified = "!";
+        staged = "+";
+        renamed = "»";
+        deleted = "✘";
+        stashed = "≡";
+      };
+      git_state = {
+        format = "\([$state( $progress_current/$progress_total)]($style)\) ";
+        style = "bright-black";
+      };
+      golang = {
+        symbol = "";
+        style = "fg:cyan";
+        format = "[$symbol ($version) ]($style)";
+      };
+      haskell = {
+        symbol = "";
+        style = "fg:#605084";
+        format = "[$symbol ($version) ]($style)";
+      };
+      java = {
+        symbol = "";
+        style = "fg:#da3b33";
+        format = "[$symbol ($version) ]($style)";
+      };
+      julia = {
+        symbol = "";
+        style = "fg:#6c82db";
+        format = "[$symbol ($version) ]($style)";
+      };
+      nodejs = {
+        symbol = "";
+        style = "fg:#95cc48";
+        format = "[$symbol ($version) ]($style)";
+      };
+      nim = {
+        symbol = "";
+        style = "fg:#ddc057";
+        format = "[$symbol ($version) ]($style)";
+      };
+      rust = {
+        symbol = "";
+        style = "fg:#fffeee";
+        format = "[$symbol ($version) ]($style)";
+      };
+      scala = {
+        symbol = "";
+        style = "fg:#cd422d";
+        format = "[$symbol ($version) ]($style)";
+      };
+      cmd_duration = {
+        format = "[$duration ]($style)";
+        style = "yellow";
+      };
+      python = {
+        format = "[$virtualenv]($style) ";
+        style = "bright-black";
+      };
+      time = {
+        disabled = false;
+        time_format = "%T"; # 24 Hour:Minute:Second Format
+        style = "fg:#626167";
+        format = "[$time]($style)";
+      };
+      fill = {
+        symbol = " ";
+      };
+    };
+  };
+
+  programs.fish = {
+    enable = true;
+
+    plugins = [
+      {
+        name = "bass";
+        src = pkgs.fetchFromGitHub {
+          owner = "edc";
+          repo = "bass";
+          rev = "2fd3d2157d5271ca3575b13daec975ca4c10577a";
+          sha256 = "0mb01y1d0g8ilsr5m8a71j6xmqlyhf8w4xjf00wkk8k41cz3ypky";
+        };
+      }
+    ];
+
+    interactiveShellInit = ''
+      set -g fish_greeting ""
+      fish_add_path ~/.local/bin
+    '';
+
+    loginShellInit = ''
+      if test -e ~/.nix-profile/etc/profile.d/nix.fish
+        source ~/.nix-profile/etc/profile.d/nix.fish
+      end
+      if test -e ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+        bass source ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+      end
+    '';
+
+    shellAliases = {
+      au  = "atuin";
+      gob = "go build .";
+      gof = "go fmt -x ./...";
+      goi = "goimports -w .";
+      gor = "go run ./...";
+      gos = "gotestsum ./...";
+      got = "go test ./...";
+      gotx = "go test -count=1 ./...";
+      vt = "./make.vsh test";
+      gc = "git checkout";
+      gb = "git branch";
+      gp = "git remote prune origin";
+      ll = "ls -la";
+      la = "ls -la";
+      l = "ls -l";
+      ".." = "cd ..";
+      "..." = "cd ../..";
+    };
+
+    functions = {
+      mkcd = "mkdir -p $argv[1]; and cd $argv[1]";
+    };
+  };
+}
---
+++ nvim/init.lua
@@ -0,0 +1,242 @@
+local globals = {
+    loaded_netrw = 1,
+    loaded_netrwPlugin = 1,
+    mapleader = ';',
+    tmux_navigator_no_mappings = 1,
+}
+for k, v in pairs(globals) do
+    vim.g[k] = v
+end
+
+vim.cmd('set nowrap')
+
+local options = {
+    ma = true,
+    mouse = "a",
+    cursorline = true,
+    tabstop = 4,
+    shiftwidth = 4,
+    softtabstop = 4,
+    expandtab = true,
+    autoread = true,
+    nu = true,
+    foldlevelstart = 99,
+    scrolloff = 7,
+    backup = false,
+    writebackup = false,
+    swapfile = false,
+    clipboard = "unnamedplus",
+    ignorecase = true,
+    smartcase = true,
+    termguicolors = true,
+    completeopt = "noinsert,fuzzy,menu,menuone,popup",
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
+
+-- theme
+require("nightfox").setup({
+    groups = {
+        carbonfox = {
+            String         = { fg = "#57d7d9" },
+            Identifier     = { fg = "#f55da9" }, -- (preferred) any variable name
+            Function       = { fg = "#ff7eb6" }, -- function name (also: methods for classes)
+            Operator       = { link = "Function" }, -- "sizeof", "+", "*", etc.
+            Keyword        = { link = "Function" }, -- any other keyword
+            Exception      = { link = "Function" }, -- try, catch, throw
+            Type           = { fg = "#ffffff" }, -- (preferred) int, long, char, etc.
+        }
+    }
+})
+vim.cmd.colorscheme "carbonfox"
+
+-- enables hover/virtual dialogs showing lines current errors
+vim.diagnostic.config({
+    virtual_text = false,
+    virtual_lines = true,
+})
+
+-- test coverage
+local goc = require('nvim-goc')
+goc.setup()
+
+vim.api.nvim_set_hl(0, 'GocCovered', {link='String'})
+vim.api.nvim_set_hl(0, 'GocUncovered', {link='Error'})
+
+-- git signs
+require('gitsigns').setup()
+
+-- terminal toggle
+require('toggleterm').setup()
+
+-- autopairs
+require('nvim-autopairs').setup()
+
+-- file in focus show relative line nums, when not show non-relative
+local augroup = vim.api.nvim_create_augroup("numbertoggle", {})
+
+vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "WinEnter" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+            vim.opt.relativenumber = true
+        end
+    end,
+})
+
+-- swap relative line numbers toggle on enter
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
+    pattern = "*",
+    group = augroup,
+    callback = function()
+        if vim.o.nu then
+            vim.opt.relativenumber = false
+            vim.cmd "redraw"
+        end
+    end,
+})
+
+-- autocomplete
+vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        vim.lsp.completion.enable(true, client.id, args.buf, {autotrigger = true})
+
+        -- Go-specific auto-formatting and import organization on save
+        if client.name == "gopls" then
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                buffer = args.buf,
+                callback = function()
+                    vim.lsp.buf.format({ async = false })
+                    OrganizeImports(1000)
+                end,
+            })
+        end
+    end
+})
+
+-- organize imports function for Go
+function OrganizeImports(timeoutms)
+    local clients = vim.lsp.get_clients({ bufnr = 0, name = "gopls" })
+    if #clients == 0 then
+        return
+    end
+
+    local client = clients[1]
+    local offset_encoding = client.offset_encoding or "utf-16"
+
+    local params = vim.lsp.util.make_range_params(0, offset_encoding)
+    params.context = { only = { "source.organizeImports" } }
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
+    for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+            if r.edit then
+                vim.lsp.util.apply_workspace_edit(r.edit, offset_encoding)
+            else
+                vim.lsp.buf.execute_command(r.command)
+            end
+        end
+    end
+end
+
+-- want autocomplete to stay open even when backspacing
+vim.api.nvim_create_autocmd("TextChangedI", {
+    callback = function()
+        local col = vim.fn.col('.')
+        local line = vim.fn.getline('.')
+        local prev_char = line:sub(col-1, col-1)
+
+        if vim.fn.pumvisible() == 0 and prev_char:match('%w') then
+            vim.lsp.completion.get()
+        elseif vim.fn.pumvisible() == 1 and prev_char == '' then
+            -- Keep menu open after backspace
+            vim.lsp.completion.get()
+        end
+    end
+})
+
+-- nvim tree setup
+require('nvim-tree').setup()
+
+local function open_nvim_tree(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+    if not directory then
+        return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
+    require('nvim-tree.api').tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+-- keybinds
+local keymap = function(tbl)
+    local opts = { noremap = true, silent = true }
+    local mode = tbl['mode']
+    tbl['mode'] = nil
+    local bufnr = tbl['bufnr']
+    tbl['bufnr'] = nil
+
+    for k, v in pairs(tbl) do
+        if tonumber(k) == nil then
+            opts[k] = v
+        end
+    end
+
+    if bufnr ~= nil then
+        vim.api.nvim_buf_set_keymap(bufnr, mode, tbl[1], tbl[2], opts)
+    else
+        vim.api.nvim_set_keymap(mode, tbl[1], tbl[2], opts)
+    end
+end
+
+nmap = function(tbl)
+    tbl['mode'] = 'n'
+    keymap(tbl)
+end
+
+imap = function(tbl)
+    tbl['mode'] = 'i'
+    keymap(tbl)
+end
+
+-- Make Enter select completion item (like Ctrl-y) when popup menu is visible
+imap { "<CR>", "pumvisible() ? '<C-y>' : '<CR>'", expr = true }
+imap { "<tab>", "pumvisible() ? '<C-y>' : '<tab>'", expr = true }
+
+-- tmux-navigator keybinds
+nmap { "<C-w>h", "<cmd>TmuxNavigateLeft<cr>" }
+nmap { "<C-w>j", "<cmd>TmuxNavigateDown<cr>" }
+nmap { "<C-w>k", "<cmd>TmuxNavigateUp<cr>" }
+nmap { "<C-w>l", "<cmd>TmuxNavigateRight<cr>" }
+
+-- telescope keybinds
+nmap{ "<leader>ff", "<cmd>Telescope find_files<cr>" }
+nmap{ "<leader>fc", "<cmd>Telescope current_buffer_fuzzy_find<cr>" }
+nmap{ "<leader>fg", "<cmd>Telescope live_grep<cr>" }
+nmap{ "<leader>fd", "<cmd>Telescope diagnostics<cr>" }
+nmap{ "<leader>fo", "<cmd>Telescope oldfiles<cr>" }
+nmap{ "<leader>fb", "<cmd>Telescope buffers<cr>" }
+nmap{ "<leader>fv", "<cmd>Telescope file_browser<cr>" }
+nmap{ "<leader>fr", "<cmd>Telescope lsp_references<cr>" }
+nmap{ "<leader>ft", "<cmd>TodoTelescope<cr>" }
+nmap{ "<leader>gb", "<cmd>Gitsigns blame_line<cr>" }
+
+-- go test coverage
+vim.keymap.set('n', '<leader>tcf', goc.Coverage, {silent=true})
+vim.keymap.set('n', '<leader>tct', goc.CoverageFunc, {silent=true})
+vim.keymap.set('n', '<leader>tcc', goc.ClearCoverage, {silent=true})
+
+-- nvim-tree keybind
+nmap{ "<leader>tt", "<cmd>NvimTreeToggle<cr>" }
+
+-- golangci-lint keybind (manual run)
+nmap{ "<leader>gl", "<cmd>lua if vim.bo.filetype == 'go' then vim.cmd('!golangci-lint run --fix') else print('golangci-lint: Not a Go file') end<cr>" }
