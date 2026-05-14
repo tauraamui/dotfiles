c91265b72899f33ffbbbf9d8d872ebbd8ca387ab
tauraamui <adampstringer@protonmail.com>
2026-05-15 00:08:24 +0100

    Revert "???"
    This reverts commit fc35a84bc23ffe8c3cc00c90a61e100afb533e42.

--- cmd/setup.v
+++ cmd/setup.v
@@ -13,7 +13,7 @@ const pkg_managers = [
 	PackageManager{
 		name: 'apt'
 		detect: ['apt-get']
-		update_cmd: 'sudo sh -c "if command -v apt >/dev/null 2>&1; then apt update; else apt-get update; fi"'
+		update_cmd: 'sudo apt-get update'
 		install_cmd: 'sudo apt-get install -y'
 	},
 	PackageManager{
